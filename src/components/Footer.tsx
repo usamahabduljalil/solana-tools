556b43de68abfed3a55009b06ccb1e727f16f13e
usamahabduljalil <usamahabduljalil21@gmail.com>
2022-08-17 16:09:59 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="w-100 pb-2">
                 <div className="flex justify-center">
 
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/ToolsSolana" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/piggydustdao" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                         <svg aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
@@ -26,7 +26,7 @@ const Footer = () => {
                         </svg>
                     </a>
 
-                    <a target="_blank" rel="noreferrer" href="https://github.com/cryptoloutre/solana-tools" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <a target="_blank" rel="noreferrer" href="https://discord.gg/Tc5reAncBK" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                         <svg aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
@@ -49,11 +49,6 @@ const Footer = () => {
                 Try on
                 <a target="_blank" rel="noreferrer" className="text-white" href="https://solanatools-devnet.vercel.app/"> <strong className="underline">Devnet</strong></a>
             </div>
-            <div className="text-center text-white text-sm pb-2">
-                Made by
-                <a target="_blank" rel="noreferrer" className="text-white" href="https://twitter.com/laloutre"> <strong>@laloutre</strong></a>
-            </div>
-            <PIPButtonCustom />
         </footer>
     );
 }

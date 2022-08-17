85649a9a3e9234102c83efac69aedcf67fe0bbd7
usamahabduljalil <usamahabduljalil21@gmail.com>
2022-08-17 13:52:46 +0100

    piggy dust tittle

--- src/pages/burnSPL.tsx
+++ src/pages/burnSPL.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = (props) => {
   return (
     <div className=" flex flex-col h-screen justify-between">
       <Head>
-        <title>Piggy Dust NFT Burn</title>
+        <title>PIGGY DUST NFT BURN TOOL</title>
         <meta name="description" content="Solana tools to help you in the solana ecosystem" />
       </Head>
       <BurnSPLView />
--- src/pages/multisender.tsx
+++ src/pages/multisender.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = (props) => {
   return (
     <div className=" flex flex-col h-screen justify-between">
       <Head>
-        <title>SOLANA TOOLS</title>
+        <title>PIGGY DUST NFT BURN TOOL</title>
         <meta name="description" content="Solana tools to help you in the solana ecosystem" />
       </Head>
       <MultiSenderView />
